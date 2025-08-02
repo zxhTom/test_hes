80c84e0159e01e411f436cdf6779c9bfd8110c30
zxhtom <870775401@qq.com>
2025-08-02 09:28:55 +0800

    add module's conftest

--- tests/kpi/test_kpi_devicestatics.py
+++ tests/kpi/test_kpi_devicestatics.py
@@ -4,25 +4,6 @@ import json
 from pytest_check import check
 
 
-@pytest.fixture
-def generate_test_pairs(pg_connect):
-    res = []
-    cur = pg_connect.cursor()
-    multiple_org_sql = "select org_no from sa.sys_org so where so.org_no like %s order by random() limit 2"
-    cur.execute(multiple_org_sql, ("100%",))
-    tables = cur.fetchall()
-    for table in tables:
-        item = {
-            "orgNo": str(table[0]),
-            "lineIds": [],
-            "tranIds": [],
-            "substationIds": [],
-            "deviceType": "01",
-        }
-        res.append(item)
-    return res
-
-
 @allure.feature("compare function")
 @allure.story("compare different group result")
 @allure.title("Compare Group")
---
+++ tests/kpi/test_kpi_onlineCycleStatusStatics.py
@@ -0,0 +1,70 @@
+import pytest
+import allure
+import json
+from pytest_check import check
+
+device_type = [
+    ("01", "/api/meters/loadDeviceInfoAndOnlineStatus"),
+    ("02", "/api/tmnl-run/page"),
+]
+meter_param = {
+    "pageNum": 1,
+    "pageSize": 2,
+    "orgNo": "100",
+    "conditions": [
+        {
+            "fieldKey": "online_status",
+            "fieldType": "String",
+            "operator": "in",
+            "values": ["GREEN"],
+        }
+    ],
+    "lineIds": [],
+    "tranIds": [],
+    "substationIds": [],
+}
+
+
+@allure.feature("compare function")
+@allure.story("compare different group result")
+@allure.title("Dynamic Group Online Statics")
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.parametrize("type,targeturl", device_type)
+def test_get_deviceTypeStatics(
+    generate_test_pairs, type, targeturl, api_client, env_config, pg_connect
+):
+    for before in generate_test_pairs:
+        before["deviceType"] = type
+        meter_param.update(before)
+        with allure.step("group payload"):
+            allure.attach(
+                body=json.dumps(before, indent=2, ensure_ascii=False),
+                name="/api/kpi/device/onlineCycleStatusStatics",
+                attachment_type=allure.attachment_type.JSON,
+            )
+        response = api_client.post(
+            "/api/kpi/device/onlineCycleStatusStatics", json=before
+        )
+        with allure.step("group response"):
+            allure.attach(
+                body=json.dumps(response.json(), indent=2, ensure_ascii=False),
+                name="/api/kpi/device/onlineCycleStatusStatics",
+                attachment_type=allure.attachment_type.JSON,
+            )
+        bt = response.json().get("data")["total"]
+        with allure.step("target param payload"):
+            allure.attach(
+                body=json.dumps(meter_param, indent=2, ensure_ascii=False),
+                name=targeturl,
+                attachment_type=allure.attachment_type.JSON,
+            )
+        response = api_client.post(targeturl, json=meter_param)
+        with allure.step("target response"):
+            allure.attach(
+                body=json.dumps(response.json(), indent=2, ensure_ascii=False),
+                name=targeturl,
+                attachment_type=allure.attachment_type.JSON,
+            )
+        meterOnlineTotal = response.json()["data"].get("total")
+        with check:
+            assert bt == meterOnlineTotal
