6e0428918a0f870d4b9138b12d80747dafac362a
zxhtom <870775401@qq.com>
2025-08-03 12:22:34 +0800

    add pgconnect keepalive avoid connection closed

--- fixtures/database.py
+++ fixtures/database.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2 import sql
 import pytest
 
+
 # 全局 token fixture
 @pytest.fixture(scope="session")
 def pg_connect(env_config):
@@ -13,6 +14,10 @@ def pg_connect(env_config):
         "user": env_config["db"]["user"],  # 如果有密码
         "password": env_config["db"]["password"],  # 自动解码为字符串
         "database": env_config["db"]["database"],
+        "keepalives": 1,  # 启用 keepalive
+        "keepalives_idle": 30,  # 空闲30秒后开始发送keepalive包
+        "keepalives_interval": 10,  # 每隔10秒发送一次keepalive
+        "keepalives_count": 3,  # 最多发送3次未响应后断开
     }
     # 建立连接
     try:
